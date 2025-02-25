e5651c1928f4f16f22717092a92680d3e3bb6363
adrianputra002 <adrian.putra002@binus.ac.id>
2025-02-25 23:23:20 +0700

    welcome to test 2 t-2

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/authenticate', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth',
+    ValidateSessionWithWorkOS::class,
+])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
